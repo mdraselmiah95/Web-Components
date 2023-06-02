338db5ce0c8e613d03a66d0fb18ee6aa2e55026b
mdraselmiah95 <russelroy15@gmail.com>
2023-06-03 00:02:05 +0600

    WIP on Web Component

--- script.js
+++ script.js
@@ -4,14 +4,32 @@ class MyButton extends HTMLElement {
     this.shadow = this.attachShadow({ mode: "open" });
   }
   connectedCallback() {
-    const button = `
-   <button>${this.innerHTML}</button>`;
-    this.innerHTML = button;
+    const button = document.createElement("button");
+    button.innerHTML = this.innerHTML;
+    this.shadow.append(this.#createStyle());
+    this.shadow.append(button);
   }
 
   attributeChangedCallBack() {}
 
   disconnectedCallback() {}
+
+  #createStyle() {
+    const style = document.createElement("style");
+    style.innerText = `
+     button{
+        padding: 0.5rem 2rem;
+        outline: none;
+        background: skyblue;
+        color: black;
+        font-weight: 600;
+        border: 1px solid #fff;
+        border-radius: 0.25rem;
+        cursor: pointer;
+     }
+    `;
+    return style;
+  }
 }
 
 customElements.define("my-element", MyButton);
