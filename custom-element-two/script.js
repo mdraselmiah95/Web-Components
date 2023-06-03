50d1fa3a0869439ceef0ddd930ced2ed899c0e8c
mdraselmiah95 <russelroy15@gmail.com>
2023-06-03 18:38:34 +0600

    WIP on Web Component

--- custom-element-two/script.js
+++ custom-element-two/script.js
@@ -1,25 +1,59 @@
 const template = document.createElement("template");
 template.innerHTML = `
-<style></style>
-<Label>
+<style>
+    div {
+        display:flex;
+        gap:1rem:
+        align-items:center;
+        padding: 1rem;
+        border: 1px solid gray;
+        margin-bottom:1rem
+    }
+    label {
+        font-family:Arial;
+        color:green;
+        font-weight:700;
+    }
+    span {
+        margin-left:auto;
+    }
+</style>
+<div>
     <input type="checkbox"/>
-    Hello World
-</label>
+    <Label>
+        <slot></slot>
+    </label>
+    <span>
+        <slot name="small"></slot>
+    </span>
+</div>
 `;
 
 class TodoItem extends HTMLElement {
   constructor() {
     super();
-    this.shadow = null;
-    this.checkbox = null;
-  }
-
-  connectedCallback() {
+    // this.shadow = null;
+    // this.checkbox = null;
     this.shadow = this.attachShadow({ mode: "open" });
     this.shadow.append(template.content.cloneNode(true));
+    this.checkbox = this.shadow.querySelector("input");
   }
 
-  attributeChangedCallback() {}
+  connectedCallback() {}
+
+  static get observedAttributes() {
+    return ["checked"];
+  }
+
+  attributeChangedCallback(name, _oldValue, newValue) {
+    if (name === "checked") {
+      this.#updateChecked(newValue);
+    }
+  }
+
+  #updateChecked(value) {
+    this.checkbox.checked = value === "true" ? true : false;
+  }
 }
 
 customElements.define("todo-item", TodoItem);
